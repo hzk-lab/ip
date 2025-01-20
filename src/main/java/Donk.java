1f3284267a974339e8ee709a995e55bfaed67d00
Zekai <leonhuangzekai@hotmail.com>
2025-01-20 13:21:14 +0800

    Level-0

---
+++ src/main/java/Donk.java
@@ -0,0 +1,22 @@
+public class Donk {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Donk\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println("Hello from\n" + logo);
+    }
+
+    /*
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+    */
+}
