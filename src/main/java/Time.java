ee2d677d31631cd88e8a05b77997e47c277c955d
Zekai <leonhuangzekai@hotmail.com>
2025-02-02 22:15:30 +0800

    Fix file storage format

--- src/main/java/Time.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-
-public class Time {
-    public String getTime(String input) {
-        LocalDate d = LocalDate.parse(input);
-    }
-}
